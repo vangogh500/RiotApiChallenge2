3592e8925153edffdcb5e11cc21d6351cdeb431c
Kai Sano Matsuda <kaisanomatsuda@Kai-Matsuda.local>
2015-08-27 16:09:54 -0400

    added basic handlebars and included it in the server script

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
 var express = require('express');
+var handlebars = require('express-handlebars').create({
+	defaultLayout:'main',
+	extname: '.hbs'
+});
 
 var app = express();
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 // Set port and ip
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP);
 
+app.get('/', function(req,res){
+	res.render('index');
+});
+
+
 // 404 page
 app.use(function(req,res){
-	res.type('text/plain');
 	res.status(404);
-	res.send('404 - Not Found');
+	res.render('404');
 });
 // 500 page
 app.use(function(err, req, res, next){
 	console.error(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error');
+	res.render('500');
 });
 
 app.listen(app.get('port'), app.get('ip'), function() {
