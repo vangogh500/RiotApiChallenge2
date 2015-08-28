625233ddb84746fabb5af692c7b0d8f5cf57d87d
Kai Sano Matsuda <kaisanomatsuda@Kai-Matsuda.local>
2015-08-27 21:27:05 -0400

    connected server script to mongodb database

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 var express = require('express');
+var app = express();
+
+// handlebars
 var handlebars = require('express-handlebars').create({
 	defaultLayout:'main',
 	extname:'.hbs'
 });
-
-var app = express();
-
 app.engine('hbs', handlebars.engine);
 app.set('view engine', 'hbs');
 
+// mongoose
+var mongoose = require('mongoose');
+var opts = {
+	server: {
+		socketOptions: { keepAlive: 1 }
+	}
+};
+
 // custom modules
 var credentials = require('./credentials.js');
+
+switch(app.get('env')) {
+	case 'development':
+		mongoose.connect(credentials.mongo.development.connectionString, opts);
+		break;
+	case 'production':
+		mongoose.connect(credentials.mongo.development.connectionString, opts);
+		break;
+	default:
+		throw new Error('Unknown execution environment: ' + app.get('env'));
+}
 
 // Set port and ip
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
