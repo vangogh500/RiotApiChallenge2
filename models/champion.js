7c7a508c4d4fc533c5e1766c861096a5789fdee5
Kai Sano Matsuda <kaisanomatsuda@Kai-Matsuda.local>
2015-08-29 14:42:12 -0400

    added method to grab brawler infos and edited champion model

--- models/champion.js
+++ models/champion.js
@@ -1,9 +1,132 @@
 var mongoose = require('mongoose');
 
 var championSchema = mongoose.Schema({
+	championId: Number,
 	name: String,
-	wins: Number,
-	picks: Number
+	imgURL: String,
+	matches: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Match'
+	}],
+	regionalStats: [{
+		regionName: String,
+		picks: Number,
+		wins: Number,
+		lanes: [{
+			lane: String,
+			match: [{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Match' 
+			}]
+		}],
+		roles: [{
+			role: String,
+			match: [{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Match' 
+			}]
+		}],
+		ranks: [{
+			rank: String,
+			match: [{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Match' 
+			}]
+		}],
+		summs: [{
+			summID: Number,
+			match: [{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Match' 
+			}]
+		}],
+		runes: [{
+			runeID: Number,
+			match: [{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Match' 
+			}]
+		}],
+		masteries: [{
+			masteryID: Number,
+			match: [{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Match' 
+			}]
+		}],
+		items: [{
+			itemID: Number,
+			match: [{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Match' 
+			}]
+		}],
+		brawlers: [{
+			brawlerID: Number,
+			match: [{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Match' 
+			}]
+		}],
+		brawlerUpgrades: [{
+			brawlerID: Number,
+			match: [{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Match' 
+			}]
+		}],
+		inGameAvgs: {
+			creepScore: Number,
+			wardsPlaced: Number,
+			wardsKilled: Number,
+			kills: Number,
+			pentaKills: Number,
+			quadraKills: Number,
+			tripleKills: Number,
+			doubleKills: Number,
+			largestMultiKill: Number,
+			largestKillingSpree: Number,
+			deaths: Number,
+			assists: Number,
+			gold: Number,
+			largestCrit: Number,
+			totalDmgDealtToChamps: Number,
+			magicDmgDealtToChamps: Number,
+			physicalDmgDealtToChamps: Number,
+			trueDmgDealtToChamps: Number,
+			magicDmgTaken: Number,
+			physicalDmgTaken: Number,
+			trueDmgTaken: Number,
+			totalDmgTaken: Number,
+			ccDurationDealt: Number,
+			totalHeal: Number,
+			neutralMinionsKilledEnemyJg: Number,
+			neutralMinionsKilledTeamJg: Number,
+			firstBloodKill: Boolean,
+			firstBloodAssist: Boolean,
+			firstTowerKill: Boolean,
+			firstTowerAssist: Boolean,
+			towerKills: Number,
+			csPerMin: {
+				zeroToTen: Number,
+				thirtyToEnd: Number,
+				tenToTwenty: Number,
+				twentyToThirty: Number
+			},
+			csDiffPerMin: {
+				zeroToTen: Number,
+				thirtyToEnd: Number,
+				tenToTwenty: Number,
+				twentyToThirty: Number
+			},
+			goldPerMin: {
+				zeroToTen: Number,
+				thirtyToEnd: Number,
+				tenToTwenty: Number,
+				twentyToThirty: Number
+			}	
+		}
+	}]
 });
 championSchema.methods.getWinRate = function(){
 	return (this.wins / this.picks);
