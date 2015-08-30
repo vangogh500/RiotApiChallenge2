9f3ea46539b20725f778378826c805fc96adcc69
Kai Sano Matsuda <kaisanomatsuda@Kai-Matsuda.local>
2015-08-30 15:25:21 -0400

    bug fix

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var opts = {
 // custom modules
 var credentials = require('./credentials.js');
 var riot = require('./lib/riot/riot.js')({
-	type: riot.types.matchWithTimeline,
+	type: 0,
 	key: credentials.riot.key,
 	region: 'NA'
 });
