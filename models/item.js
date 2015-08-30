7fdeef9b329b5e0792e9d44f1b10b1f91da2716d
Kai Sano Matsuda <kaisanomatsuda@Kai-Matsuda.local>
2015-08-30 17:38:41 -0400

    fixed bug in item model

--- models/item.js
+++ models/item.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var itemSchema = mongoose.Schema({
 	itemId: Number,
-	itemName: Number,
+	itemName: String,
 	sprite: String,
 	regionalStats: [{
 		regionName: String,
