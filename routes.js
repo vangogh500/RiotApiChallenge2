5f84f49d3b09737c5d3fad06d9c1b84e3184d423
Kai Sano Matsuda <kaisanomatsuda@Kai-Matsuda.local>
2015-08-31 02:02:06 -0400

    added REST api functionality to routes

--- routes.js
+++ routes.js
@@ -1,8 +1,26 @@
 var mainHandler = require('./handlers/main.js');
 
+var Champion = require('./models/champion.js');
+var Match = require('./models/match.js');
+
 module.exports = function(app) {
 	// index page
 	app.get('/', mainHandler.index);
+	
+	// api --------------------------------
+	app.get('/api/champions', function(req,res) {
+		Champion.find(function(err, champions) {
+			if(err) res.send(err)
+			res.json(champions);	
+		});
+	});
+	
+	app.get('/api/matches', function(req,res) {
+		Match.find(function(err, matches) {
+			if(err) res.send(err)
+			res.json(matches);
+		});
+	});
 
 	// 404 page
 	app.use(mainHandler.notFound);
