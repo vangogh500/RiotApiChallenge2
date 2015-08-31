2965b6b2aa5ce4dc2364957ca2b5cc095a56af86
Kai Sano Matsuda <kaisanomatsuda@Kai-Matsuda.local>
2015-08-31 03:26:40 -0400

    test

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,1 @@
 var app = angular.module("app", ['nvd3ChartDirectives']);
-
-app.config(function($interpolateProvider) {
-	$interpolateProvider.startSymbol('//');
-	$interpolateProvider.endSymbol('//');
-});
